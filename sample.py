64844c86f4af022b7269e4250a5404f16fc23807
smoey <e0960483@u.nus.edu>
2025-03-24 20:41:31 +0800

    Change to 3B version of qwen2.5, similar result with less processing time

--- sample.py
+++ sample.py
@@ -13,56 +13,38 @@ dataset = load_dataset("lmms-lab/AISG_Challenge")
 # Load Model and Processor
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct", 
+    "Qwen/Qwen2.5-VL-3B-Instruct", 
     torch_dtype=torch.float16,
-    device_map="auto")
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+    device_map=device)
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
 
 # Function to download video
 def download_video(youtube_url):
-    video_path = "temp.mp4"
-    ydl_opts = {
-    'format': 'best', 
-    'outtmpl': video_path, 
-    'nooverwrites': False, 
-    "getcomments": True, 
-    'extractor_args': {
-            'youtube': {
-                'max_comments': ["all","all","all","all"],
-            }
-        }
-    }
+    filename = f"{youtube_url.split("/")[-1]}.mp4"
     
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(youtube_url)
-        title = info.get("title", "Unknown Title")
-        comments = info.get("comments", [])
+    if (not os.path.exists("./videos")):
+        os.mkdir("./videos")
         
-    top_comments = sorted(comments, key=lambda x: x.get("like_count", 0), reverse=True)[:20]
-    text = "\n".join([comment["text"] for comment in top_comments])
+    video_path = f"./videos/{filename}"
     
-    conversation = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": f"Summarize these comments in a short paragraph:\n{text}"},
-            ],
-        },
-    ]
+    if (not os.path.exists(video_path)):  
+        ydl_opts = {
+            'format': 'best', 
+            'outtmpl': video_path, 
+            'nooverwrites': False
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(youtube_url)
+            title = info.get("title", "Unknown Title")
+    else:
+        ydl_opts = {
+            'quiet': True
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(youtube_url, download=False)
+            title = info.get("title", "Unknown Title")
     
-    inputs = processor.apply_chat_template(
-            conversation,
-            add_generation_prompt=True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors="pt"
-        ).to(model.device)
-    
-    output_ids = model.generate(**inputs, max_new_tokens=128)
-    generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
-    comments_summary = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
-    
-    return video_path, title, comments_summary
+    return video_path, title
 
 # Process a test case
 def process_test_case(example):
@@ -70,7 +52,7 @@ def process_test_case(example):
     question = example["question"]
     question_prompt = example["question_prompt"]
     expected_answer = example["answer"]
-    video_path, title, comments_summary = download_video(video_url)
+    video_path, title = download_video(video_url)
     
     conversation = [
         {
@@ -99,7 +81,7 @@ def process_test_case(example):
         {
             "role": "user",
             "content": [
-                    {"type": "text", "text": f"Video title: {title}\nVideo summary: {video_summary}\nComments summary: {comments_summary}\nQuestion:\n{question}\n{question_prompt}"},
+                    {"type": "text", "text": f"Video title: {title}\nVideo summary: {video_summary}\nQuestion:\n{question}\n{question_prompt}"},
                     {"type": "video", "path": video_path},
                 ],
         },
@@ -123,7 +105,7 @@ def process_test_case(example):
     print(f"Answer: {generated_answer}")
 
 # Run a test case
-sample = dataset['test'].filter(lambda x: x['qid'] == "0008-7")[0]
+sample = dataset['test'].filter(lambda x: x['qid'] == "0008-0")[0]
 process_test_case(sample)
 
 end_time = time.time()
